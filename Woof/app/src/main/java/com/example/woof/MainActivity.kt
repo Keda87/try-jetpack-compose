78a7bf892392e9182657a073f58c24e26d5edb1e
Keda87 <adiyatmubarak@gmail.com>
2022-12-30 10:53:14 +0700

    feat: enable animation on item expand woof apps

--- Woof/app/src/main/java/com/example/woof/MainActivity.kt
+++ Woof/app/src/main/java/com/example/woof/MainActivity.kt
@@ -3,6 +3,10 @@ package com.example.woof
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.StringRes
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
@@ -10,7 +14,10 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material.icons.filled.ExpandMore
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -81,49 +88,98 @@ fun WoofTopBar() {
 
 @Composable
 fun DogItemComponent(dog: Dog) {
+    var expanded by remember { mutableStateOf(false) }
     val name = stringResource(id = dog.nameResId)
+
     Card(
         elevation = 4.dp,
         modifier = Modifier
             .padding(8.dp)
     ) {
-        Row(
-            verticalAlignment = Alignment.CenterVertically,
+        Column(
             modifier = Modifier
-                .fillMaxWidth()
-                .padding(8.dp)
-                .background(MaterialTheme.colors.surface)
+                .animateContentSize(
+                    animationSpec = spring(
+                        dampingRatio = Spring.DampingRatioMediumBouncy,
+                        stiffness = Spring.StiffnessLow
+                    )
+                )
         ) {
-            Image(
-                painter = painterResource(id = dog.imageResId),
-                contentDescription = name,
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
                 modifier = Modifier
-                    .size(64.dp)
+                    .fillMaxWidth()
                     .padding(8.dp)
-                    .clip(RoundedCornerShape(50)),
-                contentScale = ContentScale.Crop,
-
-                )
-            Column(
-                modifier = Modifier.padding(start = 10.dp, end = 10.dp),
+                    .background(MaterialTheme.colors.surface)
             ) {
-                Text(
-                    text = name,
-                    style = MaterialTheme.typography.h2
-                )
-                Text(
-                    text = stringResource(id = R.string.years_old, dog.age),
-                    style = MaterialTheme.typography.body2
-                )
+                Image(
+                    painter = painterResource(id = dog.imageResId),
+                    contentDescription = name,
+                    modifier = Modifier
+                        .size(64.dp)
+                        .padding(8.dp)
+                        .clip(RoundedCornerShape(50)),
+                    contentScale = ContentScale.Crop,
+
+                    )
+                Column(
+                    modifier = Modifier.padding(start = 10.dp, end = 10.dp),
+                ) {
+                    Text(
+                        text = name,
+                        style = MaterialTheme.typography.h2
+                    )
+                    Text(
+                        text = stringResource(id = R.string.years_old, dog.age),
+                        style = MaterialTheme.typography.body2
+                    )
+                }
+                Spacer(modifier = Modifier.weight(1f))
+                ToggleItemButton(expanded = expanded, onClick = { expanded = !expanded })
+            }
+
+            if (expanded) {
+                DogHobbyComponent(dogHobbyRes = dog.hobbiesResId)
             }
         }
+    }
+}
+
+@Composable
+fun DogHobbyComponent(@StringRes dogHobbyRes: Int, modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier.padding(start = 16.dp, top = 8.dp, end = 16.dp, bottom = 16.dp)
+    ) {
+        Text(
+            text = stringResource(id = R.string.about),
+            style = MaterialTheme.typography.h3
+        )
+        Text(
+            text = stringResource(id = dogHobbyRes),
+            style = MaterialTheme.typography.body1
+        )
+    }
+}
+
+@Composable
+private fun ToggleItemButton(
+    expanded: Boolean,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    IconButton(onClick = onClick) {
+        Icon(
+            imageVector = if (expanded) Icons.Filled.ExpandLess else Icons.Filled.ExpandMore,
+            tint = MaterialTheme.colors.secondary,
+            contentDescription = stringResource(id = R.string.expand_button_content_description)
+        )
     }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    WoofTheme(darkTheme = true) {
+    WoofTheme() {
         WoofApp(dogs = Datasource.loadDogs())
     }
 }
