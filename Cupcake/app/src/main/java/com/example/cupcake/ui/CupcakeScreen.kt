3efde83752138128796df115910a8b2e404bb890
Keda87 <adiyatmubarak@gmail.com>
2022-12-31 23:58:22 +0700

    feat: Cupcake make topbar respond the navigation

--- Cupcake/app/src/main/java/com/example/cupcake/ui/CupcakeScreen.kt
+++ Cupcake/app/src/main/java/com/example/cupcake/ui/CupcakeScreen.kt
@@ -2,6 +2,7 @@ package com.example.cupcake.ui
 
 import android.content.Context
 import android.content.Intent
+import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
@@ -17,6 +18,7 @@ import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.example.cupcake.R
 import com.example.cupcake.data.Datasource
@@ -25,23 +27,28 @@ import com.example.cupcake.ui.screen.SelectOptionScreen
 import com.example.cupcake.ui.screen.SummaryScreen
 import com.example.cupcake.ui.viewmodel.OrderViewModel
 
-enum class CupcakeScreen() {
-    Start,
-    Flavor,
-    Pickup,
-    Summary
+enum class CupcakeScreen(@StringRes val title: Int) {
+    Start(R.string.app_name),
+    Flavor(R.string.choose_flavor),
+    Pickup(R.string.choose_pickup_date),
+    Summary(R.string.order_summary)
 }
 
 @Composable
-fun CupCakeAppBar(modifier: Modifier = Modifier) {
+fun CupCakeAppBar(
+    currentScreen: CupcakeScreen,
+    canNavigateBack: Boolean,
+    navigateUp: () -> Unit,
+    modifier: Modifier = Modifier
+) {
     TopAppBar(
         title = {
-            Text(text = stringResource(id = R.string.app_name))
+            Text(text = stringResource(currentScreen.title))
         },
         modifier = modifier,
         navigationIcon = {
-            if (true) {
-                IconButton(onClick = {}) {
+            if (canNavigateBack) {
+                IconButton(onClick = navigateUp) {
                     Icon(
                         imageVector = Icons.Filled.ArrowBack,
                         contentDescription = stringResource(id = R.string.back_button)
@@ -59,11 +66,17 @@ fun CupcakeApp(modifier: Modifier = Modifier) {
 
     val navController = rememberNavController()
     val viewModel: OrderViewModel = viewModel()
+    val backStackEntry by navController.currentBackStackEntryAsState()
+    val currentScreen = CupcakeScreen.valueOf(backStackEntry?.destination?.route ?: CupcakeScreen.Start.name)
 
     Scaffold(
         topBar = {
-            CupCakeAppBar()
-        }
+            CupCakeAppBar(
+                currentScreen = currentScreen,
+                canNavigateBack = navController.previousBackStackEntry != null,
+                navigateUp = { navController.navigateUp() }
+            )
+        },
     ) { padValue ->
 
         val uiState by viewModel.uiState.collectAsState()
