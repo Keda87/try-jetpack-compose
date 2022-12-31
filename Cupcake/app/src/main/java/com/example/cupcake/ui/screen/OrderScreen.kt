ce422d65fa98e1f0a2db8e1b0076fc38f4affb01
Keda87 <adiyatmubarak@gmail.com>
2022-12-31 16:07:58 +0700

    modify: Cupcake add topbar

--- Cupcake/app/src/main/java/com/example/cupcake/ui/screen/OrderScreen.kt
+++ Cupcake/app/src/main/java/com/example/cupcake/ui/screen/OrderScreen.kt
@@ -3,10 +3,9 @@ package com.example.cupcake.ui.screen
 import androidx.annotation.StringRes
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.ButtonDefaults
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
-import androidx.compose.material.TextButton
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -19,23 +18,34 @@ import com.example.cupcake.ui.theme.CupcakeTheme
 
 @Composable
 fun OrderScreen(modifier: Modifier = Modifier) {
-    Column(
-        modifier = modifier.fillMaxWidth(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center,
+    Scaffold(
+        topBar = {
+            CupCakeTopBar(
+                canGoBack = false,
+                onBackButton = { }
+            )
+        }
     ) {
-        Image(
-            painter = painterResource(id = R.drawable.cupcake),
-            contentDescription = stringResource(id = R.string.app_name),
-            modifier = Modifier.size(200.dp)
-        )
-        Text(
-            text = stringResource(id = R.string.order_cupcakes),
-            style = MaterialTheme.typography.h4
-        )
-        ButtonQuantity(onClick = { /*TODO*/ }, textResId = R.string.one_cupcake)
-        ButtonQuantity(onClick = { /*TODO*/ }, textResId = R.string.six_cupcakes)
-        ButtonQuantity(onClick = { /*TODO*/ }, textResId = R.string.twelve_cupcakes)
+        Column(
+            modifier = modifier
+                .fillMaxWidth()
+                .fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center,
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.cupcake),
+                contentDescription = stringResource(id = R.string.app_name),
+                modifier = Modifier.size(200.dp)
+            )
+            Text(
+                text = stringResource(id = R.string.order_cupcakes),
+                style = MaterialTheme.typography.h4
+            )
+            ButtonQuantity(onClick = { /*TODO*/ }, textResId = R.string.one_cupcake)
+            ButtonQuantity(onClick = { /*TODO*/ }, textResId = R.string.six_cupcakes)
+            ButtonQuantity(onClick = { /*TODO*/ }, textResId = R.string.twelve_cupcakes)
+        }
     }
 }
 
@@ -54,11 +64,36 @@ fun ButtonQuantity(
     }
 }
 
+@Composable
+fun CupCakeTopBar(
+    canGoBack: Boolean,
+    onBackButton: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    TopAppBar(
+        title = {
+            Text(text = stringResource(id = R.string.app_name))
+        },
+        modifier = modifier,
+        navigationIcon = {
+            if (canGoBack) {
+                IconButton(onClick = onBackButton) {
+                    Icon(
+                        imageVector = Icons.Filled.ArrowBack,
+                        contentDescription = stringResource(id = R.string.back_button)
+                    )
+                }
+            }
+        }
+    )
+
+}
+
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     CupcakeTheme {
-        ChooseFlavorScreen()
+        OrderScreen()
     }
 }
 
