8982d8353476d5ba1b1d69c537e293ecce2dfb46
Keda87 <adiyatmubarak@gmail.com>
2023-01-03 14:48:21 +0700

    feat: MarsPhotos app load image to a component using coil library

--- MarsPhotos/app/src/main/java/com/example/marsphotos/data/AppContainer.kt
+++ MarsPhotos/app/src/main/java/com/example/marsphotos/data/AppContainer.kt
@@ -4,20 +4,22 @@ import com.example.marsphotos.data.network.MarsApiService
 import com.example.marsphotos.data.repository.DefaultMarsPhotoRepository
 import com.example.marsphotos.data.repository.MarsPhotoRepository
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.json.Json
-import okhttp3.MediaType
+import okhttp3.MediaType.Companion.toMediaType
 import retrofit2.Retrofit
 
 interface AppContainer {
     val marsPhotoRepository: MarsPhotoRepository
 }
 
+@OptIn(ExperimentalSerializationApi::class)
 class DefaultAppContainer : AppContainer {
 
     private val baseURL = "https://android-kotlin-fun-mars-server.appspot.com"
 
     private val retrofit: Retrofit = Retrofit.Builder()
-        .addConverterFactory(Json.asConverterFactory(MediaType.get("application/json")))
+        .addConverterFactory(Json.asConverterFactory("application/json".toMediaType()))
         .baseUrl(baseURL)
         .build()
 
--- MarsPhotos/app/src/main/java/com/example/marsphotos/ui/screens/HomeScreen.kt
+++ MarsPhotos/app/src/main/java/com/example/marsphotos/ui/screens/HomeScreen.kt
@@ -1,17 +1,24 @@
 package com.example.marsphotos.ui.screens
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
 import com.example.marsphotos.R
+import com.example.marsphotos.data.model.MarsPhoto
 import com.example.marsphotos.ui.viewmodel.MarsUiState
 
 @Composable
@@ -20,7 +27,7 @@ fun HomeScreen(
     modifier: Modifier = Modifier
 ) {
     when (marsUiState) {
-        is MarsUiState.Success -> ResultScreen(photos = marsUiState.photos.size.toString())
+        is MarsUiState.Success -> ResultScreen(photos = marsUiState.photos)
         is MarsUiState.Loading -> LoadingScreen()
         is MarsUiState.Error -> ErrorScreen()
     }
@@ -52,13 +59,37 @@ fun ErrorScreen(modifier: Modifier = Modifier) {
 
 @Composable
 fun ResultScreen(
-    photos: String,
+    photos: List<MarsPhoto>,
     modifier: Modifier = Modifier
 ) {
-    Box(
-        modifier = modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
+
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(2)
     ) {
-        Text(text = photos)
+        items(photos) {
+            MarsPhotoCard(data = it)
+        }
+    }
+}
+
+@Composable
+fun MarsPhotoCard(data: MarsPhoto) {
+    Card(
+        modifier = Modifier
+            .padding(4.dp)
+            .fillMaxWidth()
+            .aspectRatio(1f),
+        elevation = 8.dp,
+    ) {
+        AsyncImage(
+            model = ImageRequest.Builder(LocalContext.current)
+                .data(data.imgSrc)
+                .crossfade(true)
+                .build(),
+            contentDescription = stringResource(id = R.string.app_name),
+            placeholder = painterResource(id = R.drawable.loading_img),
+            error = painterResource(id = R.drawable.ic_broken_image),
+            contentScale = ContentScale.FillBounds,
+        )
     }
 }
